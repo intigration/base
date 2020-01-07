a3aff4f015aee0ab31b62608b7e8eb3bb12f96de
Muhammad Farhan <muhammad.farhan@siemens.com>
2020-01-07 12:01:51 +0000

    Update app.js

--- m3qa_testfw-master/app.js
+++ m3qa_testfw-master/app.js
@@ -42,7 +42,7 @@ app.use((err, req, res, next) => {
         error: {}
     });
 });
-app.set('port', process.env.PORT || 8001);
+app.set('port', process.env.PORT || 80);
 var server = app.listen(app.get('port'), function () {
     debug('Express server listening on port ' + server.address().port);
 });
